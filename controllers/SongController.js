8527bd5434d0a300e033e7db2c9d5e42603411ca
Pablo Madrigal <pablo.madrigal@edmodo.com>
2022-01-31 12:12:02 -0600

    Primeras rutas en SongController

---
+++ controllers/SongController.js
@@ -0,0 +1,192 @@
+const Song = require("../models/SongModel");
+const { body, validationResult } = require("express-validator");
+const { sanitizeBody } = require("express-validator");
+const apiResponse = require("../helpers/apiResponse");
+const auth = require("../middlewares/jwt");
+var mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
+
+// Song Schema
+function SongData(data) {
+	this.id = data._id;
+	this.Title= data.Title;
+	this.Subtitles = data.Subtitles;
+	this.BasedOn = data.BasedOn;
+	this.SongBooks = data.SongBooks;
+	this.VerseOrder = data.VerseOrder;
+	this.SongTheme = data.SongTheme;
+	this.ChoresIntro = data.ChoresIntro;
+	this.History = data.History;
+}
+
+/**
+ * Song List.
+ * 
+ * @returns {Object}
+ */
+exports.songList = [
+	auth,
+	function (req, res) {
+		try {
+			Song.find().then((songs)=>{
+				if(songs.length > 0){
+					return apiResponse.successResponseWithData(res, "Operation success", songs);
+				}else{
+					return apiResponse.successResponseWithData(res, "Operation success", []);
+				}
+			});
+		} catch (err) {
+			return apiResponse.ErrorResponse(res, err);
+		}
+	}
+];
+
+/**
+ * Song Detail.
+ * 
+ * @param {string}      id
+ * 
+ * @returns {Object}
+ */
+exports.songDetail = [
+	auth,
+	function (req, res) {
+		if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+			return apiResponse.successResponseWithData(res, "Operation success", {});
+		}
+		try {
+			Song.findById(req.params.id).then((song)=>{                
+				if(song !== null){
+					let songData = new SongData(song);
+					return apiResponse.successResponseWithData(res, "Operation success", songData);
+				}else{
+					return apiResponse.successResponseWithData(res, "Operation success", {});
+				}
+			});
+		} catch (err) {
+			//throw error in json response with status 500. 
+			return apiResponse.ErrorResponse(res, err);
+		}
+	}
+];
+
+/**
+ * Book store.
+ * 
+ * @param {string}      title 
+ * @param {string}      subtitles
+ * @param {string}      basedOn
+ * @param {string}      songBooks
+ * @param {string}      verseOrder
+ * @param {string}      songTheme
+ * @param {string}      choresIntro
+ * @param {string}      history
+ * 
+ * @returns {Object}
+ */
+exports.bookStore = [
+	auth,
+	body("title", "Title must not be empty.").isLength({ min: 1 }).trim(),
+	body("description", "Description must not be empty.").isLength({ min: 1 }).trim(),
+	body("isbn", "ISBN must not be empty").isLength({ min: 1 }).trim().custom((value,{req}) => {
+		return Book.findOne({isbn : value,user: req.user._id}).then(book => {
+			if (book) {
+				return Promise.reject("Book already exist with this ISBN no.");
+			}
+		});
+	}),
+	sanitizeBody("*").escape(),
+	(req, res) => {
+		try {
+			const errors = validationResult(req);
+			var book = new Book(
+				{ title: req.body.title,
+					user: req.user,
+					description: req.body.description,
+					isbn: req.body.isbn
+				});
+
+			if (!errors.isEmpty()) {
+				return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
+			}
+			else {
+				//Save book.
+				book.save(function (err) {
+					if (err) { return apiResponse.ErrorResponse(res, err); }
+					let bookData = new BookData(book);
+					return apiResponse.successResponseWithData(res,"Book add Success.", bookData);
+				});
+			}
+		} catch (err) {
+			//throw error in json response with status 500. 
+			return apiResponse.ErrorResponse(res, err);
+		}
+	}
+];
+
+/**
+ * Book update.
+ * 
+ * @param {string}      title 
+ * @param {string}      description
+ * @param {string}      isbn
+ * 
+ * @returns {Object}
+ */
+exports.bookUpdate = [
+	auth,
+	body("title", "Title must not be empty.").isLength({ min: 1 }).trim(),
+	body("description", "Description must not be empty.").isLength({ min: 1 }).trim(),
+	body("isbn", "ISBN must not be empty").isLength({ min: 1 }).trim().custom((value,{req}) => {
+		return Book.findOne({isbn : value,user: req.user._id, _id: { "$ne": req.params.id }}).then(book => {
+			if (book) {
+				return Promise.reject("Book already exist with this ISBN no.");
+			}
+		});
+	}),
+	sanitizeBody("*").escape(),
+	(req, res) => {
+		try {
+			const errors = validationResult(req);
+			var book = new Book(
+				{ title: req.body.title,
+					description: req.body.description,
+					isbn: req.body.isbn,
+					_id:req.params.id
+				});
+
+			if (!errors.isEmpty()) {
+				return apiResponse.validationErrorWithData(res, "Validation Error.", errors.array());
+			}
+			else {
+				if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+					return apiResponse.validationErrorWithData(res, "Invalid Error.", "Invalid ID");
+				}else{
+					Book.findById(req.params.id, function (err, foundBook) {
+						if(foundBook === null){
+							return apiResponse.notFoundResponse(res,"Book not exists with this id");
+						}else{
+							//Check authorized user
+							if(foundBook.user.toString() !== req.user._id){
+								return apiResponse.unauthorizedResponse(res, "You are not authorized to do this operation.");
+							}else{
+								//update book.
+								Book.findByIdAndUpdate(req.params.id, book, {},function (err) {
+									if (err) { 
+										return apiResponse.ErrorResponse(res, err); 
+									}else{
+										let bookData = new BookData(book);
+										return apiResponse.successResponseWithData(res,"Book update Success.", bookData);
+									}
+								});
+							}
+						}
+					});
+				}
+			}
+		} catch (err) {
+			//throw error in json response with status 500. 
+			return apiResponse.ErrorResponse(res, err);
+		}
+	}
+];
--- models/SongModel.js
+++ models/SongModel.js
@@ -2,9 +2,39 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+var SongBookSchema = new Schema({
+	BookName: {type: String, required: true},
+	Number: {type: String, required: true},
+});
+
+var ChorSchema = new Schema({
+	Beginning: {type: Number, required: true},
+	End: {type: Number, required: true},
+	Type: {type: String, required: true}
+});
+
+var LineSchema = new Schema({
+	LineNumber: {type: Number, required: true},
+	Letter: {type: String, required: true},
+	Chores: {type: [ChorSchema]}
+});
+
+var VerseSchema = new Schema({
+	VerseName: {type: String, required: true},
+	VerseType: {type: String},
+	Lines: {type: [LineSchema], required: true}
+});
+
 var SongSchema = new Schema({
-	Nombre: {type: String, required: true},
-	Apellido: {type: String, required: true},
+	Title: {type: String, require: true},
+	Subtitles: {type: [String]},
+	BasedOn: {type: String},
+	SongBooks: {type: [SongBookSchema]},
+	VerseOrder: {type:[String], required: true},
+	SongTheme: {type:[String]},
+	ChoresIntro: {type:[ChorSchema]},
+	Verses: {type: [VerseSchema], required: true},
+	History: {type: String}
 }, {timestamps: true});
 
-module.exports = mongoose.model("Song", SongSchema);
+mongoose.model("Song", SongSchema);
