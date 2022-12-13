1af71ec566d0bfcdfc2ffa59036f0bf1c3e78698
Pablo Madrigal <pablo.madrigal@excelsoftsources.com>
2022-12-13 13:32:10 -0600

    bulk import for songs Author is in song  no appart

--- models/SongModel.js
+++ models/SongModel.js
@@ -1,57 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const SongBookSchema = new Schema({
-  BookName: {type: String, required: true},
-  Number: {type: String, required: true},
-});
+  BookName: { type: String, required: true },
+  Number: { type: String, required: true }
+})
 
 const AuthorSchema = new Schema({
-  Authors: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+  Author: { type: String, required: true },
   Type: {
     type: String,
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
 const ChorSchema = new Schema({
-  Beginning: {type: Number, required: true},
-  End: {type: Number, required: true},
+  Beginning: { type: Number, required: true },
+  End: { type: Number, required: true },
   Type: {
     type: String,
     enum: ['DO', 'RE', 'MI', 'FA', 'SOL', 'LA', 'SI'],
-    required: true,
-  },
-});
+    required: true
+  }
+})
 
 const LineSchema = new Schema({
-  LineNumber: {type: Number, required: true},
-  Letter: {type: String, required: true},
-  Chores: {type: [ChorSchema]},
-});
+  LineNumber: { type: Number, required: true },
+  Letter: { type: String, required: true },
+  Chores: { type: [ChorSchema] }
+})
 
 const VerseSchema = new Schema({
-  Type: {type: String, required: true},
-  Lines: {type: [LineSchema], required: true},
-});
+  Type: { type: String, required: true },
+  Lines: { type: [LineSchema], required: true }
+})
 
 const SongSchema = new Schema(
-    {
-      Title: {type: String, require: true},
-      Subtitles: {type: [String]},
-      BasedOn: {type: [String]},
-      SongBooks: {type: [SongBookSchema]},
-      Authors: {type: [AuthorSchema]},
-      VerseOrder: {type: [String]},
-      SongTheme: {type: [String]},
-      ChoresIntro: {type: [ChorSchema]},
-      Verses: {type: [VerseSchema]},
-      History: {type: String},
-      Active: {type: Boolean, default: true},
-      Deleted: {type: Boolean, default: false},
-    },
-    {timestamps: true},
-);
+  {
+    Title: { type: String, require: true },
+    Subtitles: { type: [String] },
+    BasedOn: { type: [String] },
+    SongBooks: { type: [SongBookSchema] },
+    Authors: { type: [AuthorSchema] },
+    VerseOrder: { type: [String] },
+    SongTheme: { type: [String] },
+    ChoresIntro: { type: [ChorSchema] },
+    Verses: { type: [VerseSchema] },
+    History: { type: String },
+    Active: { type: Boolean, default: true },
+    Deleted: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('Song', SongSchema);
+module.exports = mongoose.model('Song', SongSchema)
